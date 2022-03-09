c2d6a65331c42bc8bb92daa48cf89743e60d3894
Prudhvik-1996 <prudhvik.1996@gmail.com>
2022-03-09 12:19:18 +0530

    update theme in shared_preferences set

--- lib/src/settings/settings_service.dart
+++ lib/src/settings/settings_service.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsService {
-  Future<ThemeMode> themeMode() async => ThemeMode.light;
+  Future<ThemeMode> themeMode() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int themeIndex = prefs.getInt('THEME_INDEX') ?? 0;
+    return [ThemeMode.system, ThemeMode.light, ThemeMode.dark][themeIndex];
+  }
 
   Future<void> updateThemeMode(ThemeMode newTheme) async {
-    //  TODO preserve in shared_preferences or http
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setInt('THEME_INDEX', [ThemeMode.system, ThemeMode.light, ThemeMode.dark].indexOf(newTheme));
   }
 }
