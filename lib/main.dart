a4eb821dcf19c021311d9437805c53c31c3a598d
Prudhvik-1996 <prudhvik.1996@gmail.com>
2024-07-04 20:52:46 +0530

    Added support for both hash and path URL strategy

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,15 @@ import 'dart:html' as html;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:schoolsgo_web/src/utils/custom_routing_strategy.dart';
 
 import 'firebase_options.dart';
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
-// import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void main() async {
-  // setUrlStrategy(PathUrlStrategy());
+  setHybridUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
   await Firebase.initializeApp(
---
+++ lib/src/utils/custom_routing_strategy.dart
@@ -0,0 +1,74 @@
+import 'dart:html' as html;
+import 'dart:ui';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class HybridUrlStrategy extends UrlStrategy {
+  final UrlStrategy hashUrlStrategy;
+  final UrlStrategy pathUrlStrategy;
+
+  HybridUrlStrategy(this.hashUrlStrategy, this.pathUrlStrategy);
+
+  @override
+  String getPath() {
+    if (html.window.location.hash.isNotEmpty) {
+      return hashUrlStrategy.getPath();
+    }
+    return pathUrlStrategy.getPath();
+  }
+
+  @override
+  Object? getState() {
+    if (html.window.location.hash.isNotEmpty) {
+      return hashUrlStrategy.getState();
+    }
+    return pathUrlStrategy.getState();
+  }
+
+  @override
+  String prepareExternalUrl(String internalUrl) {
+    if (internalUrl.startsWith('#')) {
+      return hashUrlStrategy.prepareExternalUrl(internalUrl);
+    }
+    return pathUrlStrategy.prepareExternalUrl(internalUrl);
+  }
+
+  @override
+  Future<void> go(int count) async {
+    if (html.window.location.hash.isNotEmpty) {
+      await hashUrlStrategy.go(count);
+    } else {
+      await pathUrlStrategy.go(count);
+    }
+  }
+
+  @override
+  void pushState(Object? state, String title, String url) {
+    if (url.startsWith('#')) {
+      hashUrlStrategy.pushState(state, title, url);
+    } else {
+      pathUrlStrategy.pushState(state, title, url);
+    }
+  }
+
+  @override
+  void replaceState(Object? state, String title, String url) {
+    if (url.startsWith('#')) {
+      hashUrlStrategy.replaceState(state, title, url);
+    } else {
+      pathUrlStrategy.replaceState(state, title, url);
+    }
+  }
+
+  @override
+  VoidCallback addPopStateListener(EventListener fn) {
+    if (html.window.location.hash.isNotEmpty) {
+      return hashUrlStrategy.addPopStateListener(fn);
+    } else {
+      return pathUrlStrategy.addPopStateListener(fn);
+    }
+  }
+}
+
+void setHybridUrlStrategy() {
+  setUrlStrategy(HybridUrlStrategy(const HashUrlStrategy(), PathUrlStrategy()));
+}
